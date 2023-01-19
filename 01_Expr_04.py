1541dfe45181b4f3bddc9895dab8736a9a8a311d
Phitchayapha Weekate Phansamdaeng <38579614+oumphitch@users.noreply.github.com>
2023-01-19 08:49:59 +0700

    Create 01_Expr_04.py

---
+++ 01_Expr_04.py
@@ -0,0 +1,9 @@
+from math import log
+w = float(input())
+h = float(input())
+mosteller = (w*h)**0.5/60
+haycock = 0.024265*w**0.5378*h**0.3964
+boyd = 0.0333*w**(0.6157-(0.0188*log(w,10)))*h**0.3
+print(mosteller)
+print(haycock)
+print(boyd)
