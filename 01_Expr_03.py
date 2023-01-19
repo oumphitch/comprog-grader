fb100bd1e9e3c5db531c4d8da771eeecd92ddad6
Phitchayapha Weekate Phansamdaeng <38579614+oumphitch@users.noreply.github.com>
2023-01-19 08:42:27 +0700

    Update 01_Expr_03.py

--- 01_Expr_03.py
+++ 01_Expr_03.py
@@ -1,2 +1,4 @@
 from math import pi,log,exp,sin,factorial
 print(round(((pi-(factorial(10)/8**8)+(log(9.7,exp(1)))**((7/(71**0.5))-sin(40*(pi/180))))/1.2**(2.3**(1/3))),6))
+
+# ระวังหน่วย degrees กับ rad ข้อนี้ต้องเปลี่ยน degrees เป็น rad ก่อน (rad = degrees*pi/180)
